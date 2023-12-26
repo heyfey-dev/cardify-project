7a7341200b7430095334850ae6e17b5f864d1a47
Efe-money <danieokandeji@gmail.com>
2023-12-26 18:13:06 +0100

    finished validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,35 @@
 
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './App.css'
+
+
 import cardifylogo from "./images/Cardify Logo.png"
 const Login = () => {
+
+
+
+    const formik = useFormik({
+        initialValues: {
+          emailOrUsername: '',
+          password: '',
+        },
+        validationSchema: Yup.object({
+            emailOrUsername: Yup.string()
+              .required('Email or Username is required')
+              .test('emailOrUsernameFormat', 'Invalid email or username format', value => {
+                // Custom validation logic for email or username format
+                return /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})+$/.test(value);
+              }),
+            password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long'),
+          }),
+        onSubmit: values => {
+          // Handle login logic here, e.g., send data to server
+          console.log('Form values submitted:', values);
+        },
+      });
+
+
   return (
     <div>
          <div className="row justify-content-center mt-5">
@@ -14,19 +43,27 @@ const Login = () => {
                                             <div><p style={{ fontWeight:"bold"}}>Log right into the Cardify experience, with your email address or <br />username, and password.</p></div>
 
 
-              <form >
+              <form onSubmit={formik.handleSubmit}>
                             
                             <div className="mb-3">
                                     <label htmlFor="email" className="form-label" style={{ fontWeight:"bold"}}>
                                         Email or Username
                                     </label>
                                     <input
-                                        type="email"
+                                        type="text"
                                         className="form-control"
-                                        id="email"
+                                        id="emailOrUsername"
+                                        name='emailOrUsername'
                                         placeholder="Enter your email address or username"
                                         style={{padding:'15px'}}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.emailOrUsername}
                                     />
+
+                                            {formik.touched.emailOrUsername && formik.errors.emailOrUsername ? (
+                                                        <div className='error'>{formik.errors.emailOrUsername}</div>
+                                                    ) : null}
                             </div>
 
 
@@ -39,7 +76,14 @@ const Login = () => {
                                             className="form-control"
                                             id="password"
                                             style={{padding:'15px'}}
+                                            name="password"
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            value={formik.values.password}
                                         />
+                                         {formik.touched.password && formik.errors.password ? (
+                                                <div className='error'>{formik.errors.password}</div>
+                                            ) : null}
 
                                        
                             </div>
