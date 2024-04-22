60ad7e045e4ae27b321c3a6c0b90eb72b6db81aa
Your New Username <danieokandeji@gmail.com>
2024-04-22 13:34:41 +0100

    new

--- src/components/Apitext.jsx
+++ src/components/Apitext.jsx
@@ -1,9 +1,13 @@
+import cardifylogo from './images/Cardify Logo.png';
+
+
+
 const Apitext = () => {
     return (
       <div>
         <div className="card">
           <div className="card__info">
-            <div className="card__logo">MasterCard</div>
+            <div className="card__logo"><img style={{width:"90px"}} src={cardifylogo} alt="" /></div>
             <div className="card__chip">
               <svg className="card__chip-lines" role="img" width="20px" height="20px" viewBox="0 0 100 100" aria-label="Chip">
                 <g opacity="0.8">
@@ -22,14 +26,14 @@ const Apitext = () => {
             </div>
             <div className="card__type">debit</div>
             <div className="card__number">
-              <span className="card__digit-group">0123</span>
-              <span className="card__digit-group">4567</span>
-              <span className="card__digit-group">8901</span>
-              <span className="card__digit-group">2345</span>
+              <span className="card__digit-group">****</span>
+              <span className="card__digit-group">****</span>
+              <span className="card__digit-group">****</span>
+              <span className="card__digit-group">****</span>
             </div>
             <div className="card__valid-thru" aria-label="Valid thru">Valid thru</div>
             <div className="card__exp-date"><time dateTime="2038-01">01/38</time></div>
-            <div className="card__name" aria-label="Dee Stroyer">Jk Huger</div>
+            <div className="card__name" aria-label="Dee Stroyer">Cardify</div>
             <div className="card__vendor" role="img" aria-labelledby="card-vendor">
               <span id="card-vendor" className="card__vendor-sr">Mastercard</span>
             </div>
